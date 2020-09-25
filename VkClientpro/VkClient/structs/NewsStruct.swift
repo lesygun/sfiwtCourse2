260339b7b04fb6f287ccb5ab8653a19107dd0770
jalstyle <lesygun@yandex.ru>
2020-09-25 12:03:16 +0300

    Add class Session

---
+++ VkClientpro/VkClient/structs/NewsStruct.swift
@@ -0,0 +1,17 @@
+//
+//  NewsStruct.swift
+//  VkClient
+//
+//  Created by Елисей Аверкиев on 01.09.2020.
+//  Copyright © 2020 Elisey Averkiev. All rights reserved.
+//
+
+import Foundation
+
+struct News {
+    var groupName: String
+    var groupIcon: String
+    var groupData: String
+    var newsText: String
+    var newsIcon: String
+}
